704d2cf30f98f97e3d88e751fe2eea225342e63c
Dedy Martadinata S <dedyms@pm.me>
2022-07-14 21:36:19 +0700

    dont download nginx conf

--- tools/install_reactive-resume.sh
+++ tools/install_reactive-resume.sh
@@ -21,9 +21,6 @@ echo "Creating directories..."
 sudo mkdir -p /portainer/Files/AppData/Config/reactive-resume/uploads || error "Failed to create Reactive-Resume assets directory!"
 sudo mkdir -p /portainer/Files/AppData/Config/reactive-resume/db || error "Failed to create Reactive-Resume database directory!"
 
-echo "Fetching nginx configuration file"
-sudo wget -O /portainer/Files/AppData/Config/reactive-resume/nginx.conf https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/configs/reactive-resume_nginx.conf || error "Failed to download nginx.conf file"
-
 echo "Setting permissions..."
 sudo chown -R 1000:1000 /portainer/Files/AppData/Config/reactive-resume || error "Failed to set permissions!"
 
